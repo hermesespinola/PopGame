decf9b0d3328cd2034599c082f86ffb6e5cc2451
Hermes Espínola <hermesespinola@gmail.com>
2016-04-28 18:53:24 -0500

    Lcd

--- test.py
+++ test.py
@@ -11,18 +11,5 @@ button = grove.GroveButton(8)
 # Create the lcd object
 myLcd = lcd.Jhd1313m1(4, 0x3E, 0x62)
 
-def changeLCD(value, cap):
-	percentage = float(value) / capacity
-	if (percentage <= 0.25):
-		myLcd.setColor(255,0,0)
-	elif(percentage <= 0.50):
-		myLcd.setColor(255,165,0)
-	elif(percentage <= 0.75):
-		myLcd.setColor(255,255,0)
-	elif(percentage <= 1):
-		myLcd.setColor(0,255,0)
-	myLcd.setCursor(1, 0)
-	if(value < 10):
-		myLcd.write("Available: " + "0" + str(currentCapacity))
-	else:
-		myLcd.write("Available: " + str(currentCapacity))
+myLcd.setColor(255, 0, 0)
+myLcd.write("Color Rojo perras")
